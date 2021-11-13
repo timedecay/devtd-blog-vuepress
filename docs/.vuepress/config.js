8d076096072750013847b51f8d898c22847122d3
Timedecay <timedecay@foxmail.com>
2021-11-13 23:58:08 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'Hello, VuePress!',
+  description: 'some description',
+
+  themeConfig: {
+    // logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+      // NavbarItem
+      {
+        text: 'Projects',
+        link: '/projects/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/timedecay',
+      },
+    ],
+    sidebar: false,
+  },
+};
